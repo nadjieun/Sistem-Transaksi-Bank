f3ebf155e5878cd5720df1b32e56e52b855c93d3
rachmad <rachmad.eepis@gmail.com>
2018-04-14 16:24:44 +0700

    singleton done

--- src/java/singleton/SingletonApp.java
+++ src/java/singleton/SingletonApp.java
@@ -4,11 +4,44 @@
  * and open the template in the editor.
  */
 package singleton;
+import service.ServiceATM;
+import service.ServiceNasabah;
+import service.ServiceTabungan;
 
 /**
  *
  * @author USER
  */
 public class SingletonApp {
+    private static SingletonApp instance = null;
     
+    public SingletonApp getInstance(){
+        if(instance == null)
+            instance = new SingletonApp();
+        return instance;
+    }
+   
+    public SingletonApp(){
+        init();
+    }
+   
+    private ServiceTabungan serviceTabungan;
+    private ServiceNasabah serviceNasabah;
+    private ServiceATM serviceATM;
+            
+    private void init(){
+        serviceTabungan = new ServiceTabungan();
+        serviceNasabah = new ServiceNasabah();
+        serviceATM = new ServiceATM();
+    }
+    
+    public ServiceTabungan getServiceTabungan(){
+        return serviceTabungan;
+    }
+    public ServiceNasabah getServiceNasabah(){
+        return serviceNasabah;
+    }
+    public ServiceATM getServiceATM(){
+        return serviceATM;
+    }
 }
